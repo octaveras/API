cdd9e59a8b7d4cbf753a3a6411fe43c22699e401
Nicolas Machado <33378384+nmachado26@users.noreply.github.com>
2018-02-17 09:21:03 -0800

    Connect updates

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,4 @@
 //webserver
-var connect = require('connect');
-var http = require('http');
-var fs = require('fs');
 
 var express = require("express");
 var routes = express.Router();
@@ -9,13 +6,12 @@ var routes = express.Router();
 var users = require('./users');
 routes.use('/users', users);
 
+
+var requests = require('./words');
+routes.use('/words', words);
+
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Let\'s go!!!' });
 });
 
 module.exports = routes;
-
-var app = connect();
-
-http.createServer(app).listen("8888")//
-console.log("Server is now running");

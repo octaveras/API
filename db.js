e9c1bdd23b61043a986f21e7845126ff98797d69
Nicolas Machado <33378384+nmachado26@users.noreply.github.com>
2018-02-17 13:14:06 -0800

    SD

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
 
 var db_config = {
-    host: '', //port 3306
-    user: '',
-    password: '',
-    database: '',
+  host: 'octavia.c0inkp5ejyyq.us-west-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'octaviamaster',
+  password: 'octaviamasterlnrv',
+  database: 'octavia',
 };
 
 console.log('Running');

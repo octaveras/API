9f9054037d0985dc6eb6136ca2269c17f0cb5b0e
Lucas Sato <lucasjks@gmail.com>
2018-02-18 06:05:05 -0800

    Include dow and inspire (should be in prev commit)

---
+++ routes/users/dow.js
@@ -0,0 +1,49 @@
+var database = require('../../db.js');
+	module.exports = (request, response) => {
+
+	database.getConnection(function(err, connection) {
+		if (err) {
+			console.log("1");
+			console.log('error: ', err);
+			response.send("false");
+			connection.release();
+			return;
+		}
+		connection.beginTransaction(function(err) {
+			if (err) {
+				console.log("2");
+				console.log('error: ', err);
+				response.send(false);
+				connection.release();
+				return;
+			}
+			connection.query(
+				'UPDATE users\
+				 SET `dow` = ?\
+				 WHERE `id` = ?', [request.params.dow, request.params.user_id]
+			function(err) {
+					if (err) {
+						console.log("3");
+					console.log('error: ', err);
+						response.send(false);
+						connection.rollback();
+						connection.release();
+						return;
+					}
+					console.log("3 success");
+					connection.commit(function(err) {
+						if (err) {
+							console.log("4");
+							console.log('error: ', err);
+							response.send(false);
+							connection.rollback();
+							connection.release();
+							return;
+						}
+						response.send(true);
+						connection.end();
+					});
+				});
+		});
+	});
+};
